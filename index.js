7ee64c93303bbd904b78b3e22487a5ac6999886b
yuringeno <nicolecheptoo152@gmail.com>
2026-04-16 09:50:05 +0300

    first commit

--- index.js
+++ index.js
@@ -1,3 +1,20 @@
+require('datejs');
+
+function combineUsers(...args) {
+  const combinedObject = {
+    users: []
+  };
+
+  for (const arr of args) {
+    combinedObject.users = [...combinedObject.users, ...arr];
+  }
+
+  combinedObject.merge_date = new Date().toString('M/d/yyyy');
+
+  return combinedObject;
+}
+
+
 
 
 
